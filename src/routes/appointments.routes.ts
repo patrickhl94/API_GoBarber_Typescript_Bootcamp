43d991bebba829c968985f9a9d25d46592b72bd5
Patrick Lima <patrickhl94@hotmail.com>
2020-04-16 22:23:44 -0300

    Updated appointments.routes.ts

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ messege: 'WELLO WORLD!' });
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
