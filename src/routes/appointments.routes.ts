0c686fbf519c8f905bd6ee6c3c83e9e6fa61cdd4
Patrick Lima <patrickhl94@hotmail.com>
2020-04-22 23:04:42 -0300

    Updated route appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,23 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentRepository = new AppointmentRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentRepository.findByDate(parseDate);
 
   if (findAppointmentInSameDate) {
     return response
