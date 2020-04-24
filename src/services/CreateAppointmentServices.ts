17b938c892955e3a210fa6acfbc1d00368a0f780
Patrick Lima <patrickhl94@hotmail.com>
2020-04-24 00:28:42 -0300

    Updated CreatedAppoutment

---
+++ src/services/CreateAppointmentServices.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentServices {
+  private appointmentRepository: AppointmentsRepository;
+
+  constructor(appointmentRepository: AppointmentsRepository) {
+    this.appointmentRepository = appointmentRepository;
+  }
+
+  public execute({ date, provider }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already bookd');
+    }
+
+    const appointment = this.appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentServices;
