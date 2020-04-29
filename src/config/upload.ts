a71b6ab275d5aa30d267f6497bf070cd724e5956
Patrick Lima <patrickhl94@hotmail.com>
2020-04-28 22:46:09 -0300

    Updated config upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tempoFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tempoFolder,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tempoFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(8).toString('HEX');
       const fileName = `${fileHash}-${file.originalname}`;
