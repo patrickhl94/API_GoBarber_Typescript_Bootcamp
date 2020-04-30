e116458a5b1575a5d174910f4ddf87eafa56479e
Patrick Lima <patrickhl94@hotmail.com>
2020-04-29 21:27:35 -0300

    Updated services AuthencitateUser

--- src/services/AuthenticateUserServices.ts
+++ src/services/AuthenticateUserServices.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import { compare } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
 import authConfig from '../config/auth';
 import User from '../models/User';
 
@@ -22,13 +23,13 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const token = sign({}, authConfig.jwt.secret, {
