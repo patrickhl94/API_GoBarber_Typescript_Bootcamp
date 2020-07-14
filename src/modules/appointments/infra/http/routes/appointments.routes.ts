0505b8575610673a3fe470850b0ea33423d117e1
Patrick Lima <patrickhl94@hotmail.com>
2020-07-13 22:29:15 -0300

    Move files to design patern

---
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import CreateAppointmentServices from '@modules/appointments/services/CreateAppointmentServices';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
+
+//   return response.json(appointments);
+// });
+
+appointmentsRouter.post('/', async (request, response) => {
+  const { provider_id, date } = request.body;
+
+  const parseDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentServices(
+    appointmentsRepository,
+  );
+
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parseDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }),
+);
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('🚀️ Server started on port 3333 🚀️');
+});
