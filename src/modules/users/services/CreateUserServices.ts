269fb345ae995232e0bcad20c5e2c4cc79fc5f32
Patrick Lima <patrickhl94@hotmail.com>
2020-07-14 11:32:50 -0300

    Updated services CreateUser

--- src/modules/users/services/CreateUserServices.ts
+++ src/modules/users/services/CreateUserServices.ts
@@ -1,22 +1,25 @@
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
+import IUserRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   name: string;
   email: string;
   password: string;
 }
 
+@injectable()
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+  ) {}
 
-    const checkUserExist = await usersRepository.findOne({
-      where: { email },
-    });
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExist = await this.usersRepository.findByEmail(email);
 
     if (checkUserExist) {
       throw new AppError('Email address already used');
@@ -24,13 +27,11 @@ class CreateUserService {
 
     const hashedPassword = await hash(password, 8);
 
-    const user = usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password: hashedPassword,
     });
-
-    await usersRepository.save(user);
 
     return user;
   }
