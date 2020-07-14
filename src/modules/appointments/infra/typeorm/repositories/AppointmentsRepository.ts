65b58857000310a2876cc8c128b37fe9e724f2b3
Patrick Lima <patrickhl94@hotmail.com>
2020-07-13 22:27:16 -0300

    Move files to desing patern

---
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '../entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.ormRepository.findOne({
+      where: { date },
+    });
+    return findAppointment;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ date, provider_id });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
+  }
+}
+export default AppointmentsRepository;
