3840d80dffc45ab0494dcd17092cd5fb683fa643
Patrick Lima <patrickhl94@hotmail.com>
2020-07-13 22:31:55 -0300

    Updated file UpdateUserAvatarService

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -6,12 +6,12 @@ import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 
-interface Request {
+interface IRequest {
   user_id: string;
   avatarFilename: string;
 }
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const usersReopsitory = getRepository(User);
 
     const user = await usersReopsitory.findOne(user_id);
