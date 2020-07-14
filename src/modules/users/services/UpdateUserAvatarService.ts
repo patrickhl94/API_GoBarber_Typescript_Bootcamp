829e985ee7671992f1797cda8ea3037ca0d69268
Patrick Lima <patrickhl94@hotmail.com>
2020-07-14 11:33:11 -0300

    Updated service UpdatedUserAvatar

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,20 +1,26 @@
-import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
+import { inject, injectable } from 'tsyringe';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
+import IUserRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
   user_id: string;
   avatarFilename: string;
 }
-class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
-    const usersReopsitory = getRepository(User);
 
-    const user = await usersReopsitory.findOne(user_id);
+@injectable()
+class UpdateUserAvatarService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+  ) {}
+
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('Only authenticated users can change avatar', 401);
@@ -30,7 +36,7 @@ class UpdateUserAvatarService {
     }
 
     user.avatar = avatarFilename;
-    await usersReopsitory.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
