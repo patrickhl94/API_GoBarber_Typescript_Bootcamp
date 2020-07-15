14bda6de8cf751fdbfb0b17487059d9a89380f14
Patrick Lima <patrickhl94@hotmail.com>
2020-07-14 21:44:07 -0300

    Created controller User Avatar

---
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateUserAvatar from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatar);
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+}
