16607fb657885008ffc6c6e3b9d5082d0db4a435
Patrick Lima <patrickhl94@hotmail.com>
2020-08-25 11:11:07 -0300

    Created Fakje Repository UserTokens

---
+++ src/modules/users/repositories/fakes/FakeUsersTokensRepository.ts
@@ -0,0 +1,25 @@
+import { uuid } from 'uuidv4';
+
+import IUserFakeTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserFakeTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
+export default FakeUserTokensRepository;
