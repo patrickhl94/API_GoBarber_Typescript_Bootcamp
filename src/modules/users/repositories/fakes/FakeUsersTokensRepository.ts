00d6446221d685239140bb624ca8cd02fbf1a81e
Patrick Lima <patrickhl94@hotmail.com>
2020-08-27 20:06:59 -0300

    Updated fake repository FakeUsersTokens

--- src/modules/users/repositories/fakes/FakeUsersTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersTokensRepository.ts
@@ -15,6 +15,8 @@ class FakeUserTokensRepository implements IUserFakeTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
