725b591fe8873f2e11fdcf4527e2568f92b3eec9
Patrick Lima <patrickhl94@hotmail.com>
2020-08-28 11:02:26 -0300

    Updated provider shared

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,9 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorage';
 
-// import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/Implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
