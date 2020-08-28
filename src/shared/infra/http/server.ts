768192469901abedce98a12e423f736dc2b29204
Patrick Lima <patrickhl94@hotmail.com>
2020-08-28 11:02:46 -0300

    Updated serve

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -20,7 +20,7 @@ app.use(
   }),
 );
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(
